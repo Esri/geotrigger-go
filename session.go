5ca1adcadffbaaaa591eebd8183d1fb46a7a9e44
Court Fowler <courtf@gmail.com>
2014-01-05 21:25:43 -0800

    initial commit

---
+++ session.go
@@ -0,0 +1,6 @@
+package geotrigger_golang
+
+const GEOTRIGGER_BASE_URL = "https://geotrigger.arcgis.com/"
+const AGO_BASE_URL = "https://www.arcgis.com/"
+const AGO_TOKEN_ROUTE = "sharing/oauth2/token"
+const AGO_REGISTER_ROUTE = "sharing/oauth2/registerDevice"
